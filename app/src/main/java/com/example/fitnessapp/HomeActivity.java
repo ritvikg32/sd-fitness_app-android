04742aa6c6ba36d09d3aee60a9fd19a3a23330b1
Ritvik Ghattuwar <41013877+ritvikg32@users.noreply.github.com>
2021-05-07 22:06:22 +0530

    HomeActivity design completed

--- app/src/main/java/com/example/fitnessapp/HomeActivity.java
+++ app/src/main/java/com/example/fitnessapp/HomeActivity.java
@@ -2,19 +2,34 @@ package com.example.fitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.NumberPicker;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
 
     private NumberPicker numPicker;
+    private FirebaseAuth mAuth;
+    private TextView username;
+    private ImageView userdp;
+    private ImageView greetingText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mAuth = FirebaseAuth.getInstance();
 
         numPicker = findViewById(R.id.num_pick);
+        username = findViewById(R.id.username_txt);
+        userdp = findViewById(R.id.user_dp_img);
 
         numPicker.setMinValue(0);
         numPicker.setMaxValue(6);
@@ -23,6 +38,8 @@ public class HomeActivity extends AppCompatActivity {
 
         numPicker.setDisplayedValues(numPick);
 
+        updateGUI();
+
 
         numPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
@@ -30,5 +47,31 @@ public class HomeActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void updateGUI(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            String uid = user.getUid();
+
+            username.setText(name);
+            Log.d("username",name);
+
+            if(photoUrl!=null)
+                Glide.with(this).load(photoUrl).into(userdp);
+
+
+        }
     }
 }
